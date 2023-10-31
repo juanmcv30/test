3e38a88c096881383b7738f85a0dde1d89759998
Jose Garrido <juanmcv16@gmail.com>
2023-10-30 22:37:20 -0600

    Last commit

--- instance.tf
+++ instance.tf
@@ -1,9 +1,3 @@
-resource "random_password" "password" {
-  length           = var.password_length
-  special          = false
-  override_special = "!@#%^"
-}
-
 resource "google_compute_instance" "vm" {
   name         = var.instance_name
   machine_type = "n1-standard-2"
@@ -23,24 +17,62 @@ resource "google_compute_instance" "vm" {
     }
   }
 
-  metadata_startup_script = <<-EOT
-    <powershell>
-    # Create a new user
-    $secureUserPassword = ConvertTo-SecureString ${random_password.password.result} -AsPlainText -Force
-    New-LocalUser -Name ${var.admin_username} -Password $secureUserPassword -FullName "User"
-    
-    # Add the new user to the Administrators group
-    Add-LocalGroupMember -Group "Administrators" -Member ${var.admin_username}
-
-    # Install Chocolatey
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-    choco install -y office nodejs.install openjdk8 vscode git
-    Restart-Computer
-    </powershell>
-  EOT
-
   metadata = {
-    adminUsername = var.admin_username
-    adminPassword = random_password.password.result
+    windows-startup-script-ps1 = <<-EOF
+  add-type -AssemblyName System.Web
+  Add-WindowsCapability -Online -Name OpenSSH.Server
+  ssh-agent.exe
+  # Define the path to the desktop folder
+  $desktopPath = [Environment]::GetFolderPath("Desktop")
+
+  
+  # Array of usernames to add to the admin group
+  $emails = @("Fabio.rincon@arroyoconsulting.net", "andres.zapata@arroyoconsulting.net", "carlos.albarran@arroyoconsulting.net")  # Add your list of usernames here
+  
+  # Loop through the usernames and add them to the admin group
+  foreach ($email in $emails) {
+      Write-Host $emails
+      
+      $password = [guid]::NewGuid().ToString() | Get-Random
+
+      $secureString = ConvertTo-SecureString $password -AsPlainText -Force
+      $quotedSecureString = $secureString 
+
+      $name = $email.Split("@")[0]
+      
+      New-LocalUser -Name $name -Password $quotedSecureString -Description "SSH User: $email"
+      New-Item -Path "C:\Users\$name\.ssh" -ItemType Directory
+  
+      # Add the user to the admin group
+      Add-LocalGroupMember -Group "Administrators" -Member $name
+
+      Start-Process ssh-keygen -b 4096 -f C:\Users\$name\.ssh\$name -C $email -N $passphrase '' -Wait
+
+      $authorizedKeysFile = "C:\Users\$name\.ssh\authorized_keys"
+      Add-Content -Path $authorizedKeysFile -Value (Get-Content "C:\Users\$name\.ssh\key.pub")
+
+     
   }
+  
+
+  # Install Chocolatey
+  Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+  
+  # Install Office, Node.js, Java, Visual Studio Code, and GIT
+  choco install nodejs openjdk microsoft-office-deployment javaruntime vscode git -y
+  
+  # Set Environment Variables for Java
+  #$javaPath = (Get-Command java).Source
+  #[Environment]::SetEnvironmentVariable('JAVA_HOME', $javaPath, [System.EnvironmentVariableTarget]::Machine)
+  #[Environment]::SetEnvironmentVariable('Path', "$($env:Path);$javaPath\bin", [System.EnvironmentVariableTarget]::Machine)
+  
+ 
+  # Verify installations
+  Write-Host "Chocolatey, Office, Node.js, Java, Visual Studio Code, and GIT have been installed."
+  
+
+  EOF
+  }
+
+
 }
--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,3 @@ output "external_ip" {
   value = google_compute_instance.vm.network_interface.0.access_config.0.nat_ip
 }
 
-output "admin_username" {
-  value = var.admin_username
-}
-output "admin_password" {
-  value = random_password.password.result
-  sensitive = true
-}
